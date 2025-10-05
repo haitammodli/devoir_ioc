800d11d1a264be75c98d2e189a57411c7c16c5c8
HaitamP <haytam.modli2004@gmail.com>
2025-10-05 14:32:38 +0100

    update v2

--- src/main/java/pres/SpringXML.java
+++ src/main/java/pres/SpringXML.java
@@ -1,13 +1,14 @@
 package pres;
 
+import metier.Gestion;
 import metier.IGestion;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SpringXML {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        IGestion gestion = context.getBean(IGestion.class);
+        ApplicationContext context = new ClassPathXmlApplicationContext("SpringConfig.xml");
+        Gestion gestion = (Gestion) context.getBean("metier");
         System.out.println("Résultat: " + gestion.calcul());
     }
 }
